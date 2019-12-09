d1413589f72986da1dcfab170d30c8069609b331
Eder Canada <canada.eder@gmail.com>
2019-12-09 13:58:14 -0800

    set up home model

---
+++ models/homes.js
@@ -0,0 +1,22 @@
+// calling mongoose
+const mongoose = require('mongoose')
+
+// Schema is a blueprint that my user object will follow 
+const Schema = mongoose.Schema;
+
+const homeSchema = new Schema({
+  addrees: {type: String, require: true},
+  city: {type: String, require: true},
+  state: {type: String, require: true},
+  salePrice: {type: String, require: true},
+  img: {type: String, require: true},
+  description: {type: String, require: true}
+})
+
+
+// creating my model and calling it User
+const Home = mongoose.model('Home',homeSchema)
+
+
+// exporting my model
+module.exports = Home 
