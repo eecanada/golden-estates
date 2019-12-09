c51b50b392a6671883c0a3ffaee357f8f6cf1709
Eder Canada <canada.eder@gmail.com>
2019-12-09 15:04:57 -0800

    edit home create route to pushhome to user

--- controllers/homes.js
+++ controllers/homes.js
@@ -7,6 +7,9 @@ router.post('/', async  (req, res) => {
   try {
     console.log(req.body)
     const createdHome = await Home.create(req.body)
+    const foundUser = await User.findById(req.body.userId)
+    const [findUser, createBook] = await ([foundUser,createdBook]);
+    findUser.homes.push(createBook)
     res.json(createdHome)
   } catch (err){
     res.send(err)
