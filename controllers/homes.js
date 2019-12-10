35530d0d5db2ef46ebe5ce9378c46c89664f4597
Eder Canada <canada.eder@gmail.com>
2019-12-09 16:03:58 -0800

    edit home create route to push a home into the user that created it

--- controllers/homes.js
+++ controllers/homes.js
@@ -1,6 +1,8 @@
 const express =  require('express')
 const router = express.Router()
 const Home = require('../models/homes')
+const User = require('../models/users')
+
 
 // create a home listing
 router.post('/', async  (req, res) => {
@@ -8,15 +10,16 @@ router.post('/', async  (req, res) => {
     console.log(req.body)
     const createdHome = await Home.create(req.body)
     const foundUser = await User.findById(req.body.userId)
-    const [findUser, createBook] = await ([foundUser,createdBook]);
-    findUser.homes.push(createBook)
+    const [findUser, createHome] = await ([foundUser,createdHome]);
+    findUser.homes.push(createHome)
+    await findUser.save();
     res.json(createdHome)
   } catch (err){
     res.send(err)
   };
 });
 
-
+// will get all homes 
 router.get('/', async (req,res)=>{
   try{
     const allHomes = await Home.find({})
@@ -26,5 +29,14 @@ router.get('/', async (req,res)=>{
   }
 });
 
+// this will allow me to edit my home 
+router.put('/:id', async (req,res)=>{
+  try{
+    const updatedBook = await Book.findByIdAndUpdate(req.params.id, req.body, {new:true});
+    res.redirect('/books')
+  } catch (err){
+    res.send(err)
+  }
+})
 
 module.exports = router
