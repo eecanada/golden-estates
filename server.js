03d42070c2aa1c8203737bb606522256270e39a1
Eder Canada <canada.eder@gmail.com>
2019-12-10 11:21:42 -0800

    set up session in the backend for registration

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
 const cors = require("cors")
-
+const methodOverride = require('method-override')
+const session = require('express-session')
 
 
 const usersController = require('./controllers/users')
@@ -17,16 +18,23 @@ require('./database/database')
 //middleware
 app.use(cors());
 app.options("http://localhost:3000", cors())
-app.use(express.static("public"))
-app.use(express.json())
+app.use(express.static("public")) //
+app.use(express.json()) //
+app.use(methodOverride('_method')); //
+
+app.use(session({
+  secret: "this is a random secret string",
+  resave: false,
+  saveUninitialized: false 
+}))
 
 // taking json that is sent from the client,to the server and it is going to parse it 
 app.use(bodyParser.json())
 
 
-// this doesnt have to match
+// this is my home page
 app.get('/', (req, res) => {
-  res.send('hit the home route!')
+  res.send()
 })
 
 
@@ -59,6 +67,9 @@ app.post("/", cors(), async (req,res)=>{
   })
 
   // setting up my user route
+  app.get('/registration', (req,res)=>{
+    res.render()
+  })
  
   // // telling express app to use this route 
   app.use('/users',usersController)
--- src/components/RegisterUser/index.js
+++ src/components/RegisterUser/index.js
@@ -1,54 +1,54 @@
-import React, {Component}  from 'react'
+// import React, {Component}  from 'react'
 
 
-class Register extends Component{
-    state = {
-        username: '',
-        email: '',
-        password: ''
-    }
+// class Register extends Component{
+//     state = {
+//         username: '',
+//         email: '',
+//         password: ''
+//     }
 
-    onChange = e =>                          
-        this.setState({
-            [e.target.name]: e.target.value
-        })
+//     onChange = e =>                          
+//         this.setState({
+//             [e.target.name]: e.target.value
+//         })
 
-    onSubmit = async e => {
-        e.preventDefault()
-        const { username, email, password } = this.state
-        // bcrypt password 
-        // fetch call - await 
-          // method POST 
-          // content-type
-          // headers
-          // body 
-        // set authentication 
-        // props.history.push
+//     onSubmit = async e => {
+//         e.preventDefault()
+//         const { username, email, password } = this.state
+//         // bcrypt password 
+//         // fetch call - await 
+//           // method POST 
+//           // content-type
+//           // headers
+//           // body 
+//         // set authentication 
+//         // props.history.push
          
-    }
+//     }
 
-    render() {
-        // const { email, password, passwordT, error } = this.state
-        // const isInvalid =
-        //     password !== passwordTwo ||
-        //     password === '' ||
-        //     email === ''
-        return (
-            <div>
-                <h1>SIGNUP</h1>
-                <form onSubmit={this.onSubmit}>
-                    <input type='text' name='email' value={email} placeholder="EMAIL" onChange={this.onChange}/>
-                    <input type='text' name='password' value={password} placeholder="PASSWORD" onChange={this.onChange}/>
-                    <input type='text' name='passwordTwo' value={passwordTwo} placeholder="PASSWORD" onChange={this.onChange}/>
-                    <input type='submit' value='submit' disabled={isInvalid}/>
-                    {error && <p>{error.message}</p>}
-                </form>
-            </div>
-        )
-    }
-}
-
-
+//     render() {
+//         // const { email, password, passwordT, error } = this.state
+//         // const isInvalid =
+//         //     password !== passwordTwo ||
+//         //     password === '' ||
+//         //     email === ''
+//         return (
+//             <div>
+//                 <h1>SIGNUP</h1>
+//                 <form onSubmit={this.onSubmit}>
+//                     <input type='text' name='email' value={email} placeholder="EMAIL" onChange={this.onChange}/>
+//                     <input type='text' name='password' value={password} placeholder="PASSWORD" onChange={this.onChange}/>
+//                     <input type='text' name='passwordTwo' value={passwordTwo} placeholder="PASSWORD" onChange={this.onChange}/>
+//                     <input type='submit' value='submit' disabled={isInvalid}/>
+//                     {error && <p>{error.message}</p>}
+//                 </form>
+//             </div>
+//         )
+//     }
+// }
 
 
-export default Register
+
+
+// export default Register
